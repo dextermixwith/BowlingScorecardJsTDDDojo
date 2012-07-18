ff3dad8339d66460b8992849d82ef601bdecc220
Esendex Developer <esendexdev@gmail.com>
2012-07-18 15:04:46 +0100

    GOt Chutzpah working using Sinon

--- WebApp.Tests/tests/scorecard.tests.js
+++ WebApp.Tests/tests/scorecard.tests.js
@@ -1,4 +1,9 @@
+/// <reference path="../../WebApp/assets/js/jquery-1.7.2.min.js" />
 /// <reference path="../../WebApp/assets/js/scorecard.js" />
+/// <reference path="../sinon/sinon-1.4.2.js" />
+
+var scoreCard = new ScoreCard();
+
 test("Creating a scorecard has current score value of 0", function () {
-	equal(new ScoreCard().CurrentScore, 0, "Current Score Should be 0");
+    equal(scoreCard.CurrentScore, 0, "Current Score Should be 0");
 });
---
+++ WebApp/assets/js/pushregistrations.js
@@ -0,0 +1,12 @@
+﻿var EditablePushNotificationRegistrationRow = function (rowNodeCollection, jQueryObject) {
+    this.internalJQuery = jQueryObject || $;
+    this.initialiseRows(rowNodeCollection);
+};
+
+
+EditablePushNotificationRegistrationRow.prototype = {
+    initialiseRows: function (rowNodeCollection) {
+        this.internalJQuery('a[name="edit"]', rowNodeCollection).click(function () { });
+        this.internalJQuery('a[name="clear"]', rowNodeCollection).click(function () { });
+    }
+};
