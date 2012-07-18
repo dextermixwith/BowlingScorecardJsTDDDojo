2e40047755fa74ba0af8338d5414b87fb752dbe6
John Armstrong-Prior <jarmstrongprior@googlemail.com>
2012-07-18 22:17:19 +0100

    Initial jQuery related tests

--- WebApp.Tests/tests/scorecard.tests.js
+++ WebApp.Tests/tests/scorecard.tests.js
@@ -1,4 +1,23 @@
 /// <reference path="../../WebApp/assets/js/scorecard.js" />
-test("Creating a scorecard has current score value of 0", function () {
-	equal(new ScoreCard().CurrentScore, 0, "Current Score Should be 0");
+
+var scorecardTableNodeCollection = $('<table class="scorecardTable"><thead><tr><th colspan="2">1</th></tr></thead><tbody><tr><td>-</td><td>-</td></tr><tr><td class="turnScore" colspan="2">-</td></tr></tbody></table>');
+
+var stubbedJQuery;
+var scorecard;
+
+module("Score Card Initialisation", {
+	setup : function() {
+		stubbedJQuery = sinon.stub();
+		scorecard = new ScoreCard(scorecardTableNodeCollection, stubbedJQuery);		
+	}
 });
+
+
+test("Creating a scorecard has current turn set to 1", function () {
+	equal(scorecard.CurrentTurn, 1, "Current turn should be 1");
+});
+
+test("Turn score cells are selection test", function() {
+	ok(stubbedJQuery.calledWith("td.turnScore"), "turn score cells in table are selected.");
+});
+
--- WebApp/assets/js/scorecard.js
+++ WebApp/assets/js/scorecard.js
@@ -1,7 +1,11 @@
-var ScoreCard = function () {
-    this.CurrentScore = 0;
+var ScoreCard = function (scoreCardTableNode, internalJQuery) {
+    this.CurrentTurn = 1;
+    this.internalJQuery = internalJQuery || $;
+    this.initialiseTable(scoreCardTableNode);
 };
 
 ScoreCard.prototype = {
-    
+    initialiseTable : function(scoreCardTableNode) {
+   		this.internalJQuery("td.turnScore", scoreCardTableNode);
+    }
 };
