7658de5c0919d10f1e67e61fca51d0f964008496
John Armstrong-Prior <jarmstrongprior@googlemail.com>
2012-08-15 18:17:50 +0100

    Tweaked some styling on turn input fields. Now clears selected table cell and puts focus on input field

--- WebApp.Tests/tests/scorecard.tests.js
+++ WebApp.Tests/tests/scorecard.tests.js
@@ -13,7 +13,7 @@ module("Score Card Initialisation", {
 		stubbedJQueryProxy = sinon.stub({ proxy: function() {} });
 		turnScoreCellSelectionResultStub = sinon.stub( { on : function() {} } );
 		stubTurnScoreClickEventHandler = sinon.stub();
-		stubbedJQuery.withArgs("td.turnScore").returns(turnScoreCellSelectionResultStub);
+		stubbedJQuery.returns(turnScoreCellSelectionResultStub);
 		scorecard = new ScoreCard(scorecardTableNodeCollection, stubbedJQuery, stubTurnScoreClickEventHandler, stubbedJQueryProxy);		
 	}
 });
@@ -34,12 +34,57 @@ test("The turn score cell event click proxy test", function() {
 	ok(stubbedJQueryProxy.proxy.calledWith(stubTurnScoreClickEventHandler), "The turn score cell event click handler is proxied by jQuery");
 });
 
+var clickEvent, injectedJQueryForProxy, currentTurnCellStub, turnInlineInputStub;
+
 module("Score Card Turn Click", {
 	setup : function() {
-		stubbedJQuery = sinon.stub({ proxy : function() {} } );
-		turnScoreCellSelectionResultStub = sinon.stub( { on : function() {} } );
-		stubTurnScoreClickEventHandler = sinon.stub();
+		currentTurnCellStub = sinon.stub({ append : function () {} , on : function() {} });
+		turnInlineInputStub = sinon.stub({ on : function() {}, trigger : function() {} });
+		clickEvent = sinon.stub({ currentTarget : currentTurnCellStub });	
+		turnScoreCellSelectionResultStub = sinon.stub( { 
+												append : function() {}, 
+												on : function() {} , 
+												empty : function() {},
+
+											});
+		
+		stubbedJQuery = sinon.stub();
 		stubbedJQuery.withArgs("td.turnScore").returns(turnScoreCellSelectionResultStub);
-		scorecard = new ScoreCard(scorecardTableNodeCollection, stubbedJQuery).turnScoreClickEventHandler();	
+		stubbedJQuery.withArgs(currentTurnCellStub).returns(turnScoreCellSelectionResultStub);
+		stubbedJQuery.withArgs('<input type="text" class="turnInput" maxlength="1" />').returns(turnInlineInputStub);
+
+		scorecard = new ScoreCard(scorecardTableNodeCollection, stubbedJQuery).turnScoreClickEventHandler(clickEvent);	
 	}
+});
+
+test("Click event select find current turn cell test", function() {
+	ok(stubbedJQuery.calledWith(currentTurnCellStub), "current turn cell is found by jQuery");
+});
+
+test("Turn cell inline input element creation tests", function() {
+	ok(stubbedJQuery.calledWith('<input type="text" class="turnInput" maxlength="1" />'), "turn inline input element has been created by jQuery");
+});
+
+test("Current turn cell append test", function() {
+	ok(turnScoreCellSelectionResultStub.append.called, "append is called on turn score cell element");
+});
+
+test("Current turn cell contents cleared test", function() {
+	ok(turnScoreCellSelectionResultStub.empty.called, "empty is call on turn score cell");
+});
+
+test("Current turn cell add input test", function() {
+	ok(turnScoreCellSelectionResultStub.append.calledWith(turnInlineInputStub), "inline input is added to turn cell");
+});
+
+test("Current turn cell focused input test", function() {
+	ok(turnInlineInputStub.trigger.calledWith("focus"), "input input gets focus");
+});
+
+test("Turn cell input focus loss event attachment tests", function() {
+	ok(turnInlineInputStub.on.calledWith("blur"));
+});	
+
+test("Turn cell input keydown event attachment tests", function() {
+	ok(turnInlineInputStub.on.calledWith("keydown"));
 });
--- WebApp/assets/js/scorecard.js
+++ WebApp/assets/js/scorecard.js
@@ -1,7 +1,7 @@
-var ScoreCard = function (scoreCardTableNode, internalJQuery, injectedTurnScoreClickEventHandler, injectedJQueryProxy) {
+var ScoreCard = function (scoreCardTableNode, internalJQuery, injectedTurnScoreClickEventHandler, injectedJQueryForProxy) {
     this.CurrentTurn = 1;
     this.internalJQuery = internalJQuery || $;
-    this.jQueryForProxying = injectedJQueryProxy || $;
+    this.jQueryForProxying = injectedJQueryForProxy || $;
     this.turnScoreClickEventHandler = injectedTurnScoreClickEventHandler || this.turnScoreClickEventHandler ;
     this.initialiseTable(scoreCardTableNode);
 };
@@ -12,6 +12,11 @@ ScoreCard.prototype = {
    			.on("click", this.jQueryForProxying.proxy(this.turnScoreClickEventHandler, this));
     },
     turnScoreClickEventHandler : function(event) {
-
+        var turnInlineInput = this.internalJQuery('<input type="text" class="turnInput" maxlength="1" />');
+        turnInlineInput.on("blur", function(){});
+        turnInlineInput.on("keydown", function(){})
+        this.internalJQuery(event.currentTarget).empty();
+    	this.internalJQuery(event.currentTarget).append(turnInlineInput);
+        turnInlineInput.trigger("focus");
     }
 };
